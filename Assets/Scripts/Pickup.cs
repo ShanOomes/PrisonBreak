60af1b219f2c0212a04542f2fe1f86337de6b7ec
Shan <shanoomes@gmail.com>
2021-02-04 14:58:15 +0100

    Inplementing Pickups, interactables interface

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour, IInteractable
+{
+    public void Action(PlayerManager player)
+    {
+
+    }
+}
