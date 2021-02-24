a4b972d4fa93d8f54339a9a829615a5ab5c23fab
Shan <shanoomes@gmail.com>
2021-02-24 11:26:15 +0100

    Added level design

--- Assets/Scripts/Interactables/SlidingDoor.cs
+++ Assets/Scripts/Interactables/SlidingDoor.cs
@@ -5,16 +5,19 @@ using DG.Tweening;
 
 public class SlidingDoor : Door
 {
+    public GameObject left;
+    public GameObject right;
     void Update()
     {
         if (open)
         {
-            gameObject.transform.DOLocalMoveZ(1.45f, 1f, false);
-
+            left.transform.DOLocalMoveZ(1.45f, 1f, false);
+            right.transform.DOLocalMoveZ(-2.90f, 1f, false);
         }
         else
         {
-            gameObject.transform.DOLocalMoveZ(0, 1f, false);
+            left.transform.DOLocalMoveZ(0, 1f, false);
+            right.transform.DOLocalMoveZ(-1.45f, 1f, false);
         }
     }
     public void ResetDoor()
