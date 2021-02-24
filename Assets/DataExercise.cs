02fe82d8b7b887c9877ad28ffa1429a0f316acfc
Shan <shanoomes@gmail.com>
2021-02-24 15:09:55 +0100

    Created dictionary and list exercise

---
+++ Assets/DataExercise.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reading
+{
+    
+}
+public class DataExercise : MonoBehaviour
+{
+    private List<string> words;
+    private Dictionary<float, Reading> sensorData; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        sensorData = new Dictionary<float, Reading>();
+    }
+
+    IEnumerator SensorReading()
+    {
+        //while(sensorData < 100)
+        {
+            float time = Time.realtimeSinceStartup;
+            //sensorData.Add(time, new Reading(Random.value, Random.value, Random.value));
+
+            Reading r = sensorData[time];
+            print("Reading: " + r);
+            yield return new WaitForSeconds(5);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
