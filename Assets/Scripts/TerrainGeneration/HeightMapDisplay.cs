ffb5c81827ba02d22171f6f724621b0fd8e188ff
Shan <shanoomes@gmail.com>
2021-03-11 15:21:12 +0100

    TerrainGeneration scripts

---
+++ Assets/Scripts/TerrainGeneration/HeightMapDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightMapDisplay : TerrainConfig
+{
+    public Material mat = null;
+
+    protected override void UpdateTerrainData(float[,] data)
+    {
+        if (mat != null)
+        {
+            mat.mainTexture = ProceduralUtils.GenerateTexture2D(data);
+            mat.mainTexture.filterMode = FilterMode.Point;
+            mat.mainTexture.wrapMode = TextureWrapMode.Clamp;
+        }
+    }
+}
