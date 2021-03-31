790ff8b98415f6a1f8b2fbb78acbef7470a9cd86
ShanOomes <shanoomes@gmail.com>
2021-03-31 13:01:41 +0200

    Added an Compass

---
+++ Assets/Scripts/Compass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    public Transform playerPos;
+    Vector3 dir;
+    // Update is called once per frame
+    void Update()
+    {
+        dir.z = playerPos.eulerAngles.y;
+        transform.localEulerAngles = dir;
+    }
+}
